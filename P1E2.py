e811e45e72daa443056f8327bbe4f1484c3a02ce
Germán Gómez Porras <47105994+Ggomezporras@users.noreply.github.com>
2019-01-29 22:20:22 +0100

    Update P1E2.py

--- P1E2.py
+++ P1E2.py
@@ -1,1 +1,32 @@
+# Declaración de las tres listas
+strList=["Pasagarda", "Susa", "Persépolis"]
+intList=[5,3,6]
+varList=["Botella", 5, 5.2]
 
+#Print de las tres listas
+print (strList)
+print (intList)
+print (varList)
+
+#Asignacion de los ultimos valores de cada lista a variables
+lastStr = strList[-1]
+lastInt = intList[-1]
+lastVar = varList[-1]
+
+#Print de un texto y la concatenacion de variables de distinto tipo
+print ("No se si entiendo muy bien este apartado",lastStr, str(lastInt), str (lastVar))
+
+#Declaración de un diccionario
+peliculas ={
+    "Aronofsky": "Black Swan",
+    "Borges": "El Aleph"
+}
+
+#print del diccionario, sus claves y sus valores
+print (peliculas)
+
+for key in peliculas:
+    print (key)
+
+for key in peliculas:
+    print (peliculas[key])
