5764c31db6a300b8f9cdab8c1349ded7fe75903c
Germán Gómez Porras <47105994+Ggomezporras@users.noreply.github.com>
2019-01-31 11:53:50 +0100

    Update and rename P1E3.py to 03_personal-info.py

---
+++ 03_personal-info.py
@@ -0,0 +1,14 @@
+#introducir los datos
+print ("Please introduce the following data about yourself.")
+print ("Name:")
+name = input()
+print ("Surname:")
+surname = input()
+print ("Telephone:")
+phone = input()
+print ("City:")
+city = input()
+print ("Age:")
+age = input()
+# print de los resultados en una frase con sentido
+print ("You are ", name, surname, "from", city, ". You are", age, "years old and your number is", phone)
